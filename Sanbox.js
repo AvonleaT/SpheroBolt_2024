8146959cd267ddc7331a394731509b85011cdd84
Thalmann <thalmann.avonlea@gmail.com>
2024-01-18 07:54:38 -0500

    Moved Figure 8 Down

--- Sanbox.js
+++ Sanbox.js
@@ -2,6 +2,7 @@
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing 
 //Bolt ID: 52F9
+/*
 async function startProgram() {
 	await speak("Hello World", true);
 	setMainLed({ r: 0, g: 0, b: 255});
@@ -9,4 +10,22 @@ async function startProgram() {
 	await delay (2);
 	setSpeed(0);
 
+
+*/
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
+*/
+
+// Square w/ different colors for each line & commit
+
+
+
+// Square w/ different colors & sounds for each line & commit
+
+
+// Figure 8 & commit

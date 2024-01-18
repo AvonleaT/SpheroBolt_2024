5b0329b061b9205de11b9f15695f508f1251182e
Thalmann <thalmann.avonlea@gmail.com>
2024-01-18 08:01:04 -0500

    Deleted The Copied Code

--- Sanbox.js
+++ Sanbox.js
@@ -9,15 +9,6 @@ async function startProgram() {
 	setSpeed(60);
 	await delay (2);
 	setSpeed(0);
-
-
-*/
-async function startProgram() {
-	await speak("Hello World", true);
-	setMainLed({ r: 0, g: 0, b: 255});
-	setSpeed(60);
-	await delay (2);
-	setSpeed(0);
 }
 */
 

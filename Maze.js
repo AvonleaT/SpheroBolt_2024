451d8017c7c55defab463922725f78803f6658a0
Thalmann <thalmann.avonlea@gmail.com>
2024-01-22 08:38:35 -0500

    Added Comment Above Start Code

--- Maze.js
+++ Maze.js
@@ -3,6 +3,8 @@
 // Date: 1.22.2024
 // Program: Maze
 // Bolt ID: 52F9
+
+//Start message
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);

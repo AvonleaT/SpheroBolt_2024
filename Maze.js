9d99a87e88beb881c756fd0e6a8f028007dc3d1a
Thalmann <thalmann.avonlea@gmail.com>
2024-01-22 08:37:26 -0500

    Start Maze  (1.22.2024)
    The Sphero Bolt says "Start" and the word "start" is displayed on the screen.

--- Maze.js
+++ Maze.js
@@ -3,4 +3,7 @@
 // Date: 1.22.2024
 // Program: Maze
 // Bolt ID: 52F9
-
+async function startProgram() {
+await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
+await speak("Start", true);
+}

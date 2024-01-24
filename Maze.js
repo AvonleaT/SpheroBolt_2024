c495f6cbfedaa2714ce5c3571ac0195ab5a91dcb
Thalmann <thalmann.avonlea@gmail.com>
2024-01-24 08:28:28 -0500

    1.24.2024
    Working on checkpoint 1

--- Maze.js
+++ Maze.js
@@ -4,8 +4,17 @@
 // Program: Maze
 // Bolt ID: 52F9
 
-//Start message
+/*
+// Start Maze
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
+*/
+
+//Blue: Checkpoint 1
+async function startProgram() {
+	await roll(0, 50, 5)
+	await delay (3)
+	setMainLed({ r: 0, g: 0, b: 255 });
 }
+
